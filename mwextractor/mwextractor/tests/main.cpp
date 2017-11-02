bbd8fd83adedcff313a96944f0d4bb50e1134a0d
Igor <igor.danielewicz@gmail.com>
2017-11-02 18:47:44 +0100

    Added global fixture to test.

--- mwextractor/mwextractor/tests/main.cpp
+++ mwextractor/mwextractor/tests/main.cpp
@@ -1,12 +1,9 @@
-/**
-\mainpage Strona główna projektu MWeXtractor
 
-MWeXtractor to biblioteka programistyczna oraz zestaw narzędzi
-i skryptów przeznaczonych do ekstrakcji wyrażeń wielowyrazowych
-z tekstów, a także badań nad metodami wydobywania kolokacji.
-*/
 #define BOOST_TEST_MODULE measures_test
+
+// #define BOOST_TEST_NO_MAIN
 #define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_ALTERNATIVE_INIT_API
 
 #include <unordered_set>
 
@@ -30,15 +27,40 @@ z tekstów, a także badań nad metodami wydobywania kolokacji.
 
 #include <boost/test/unit_test.hpp>
 
+
 using namespace function::association;
+
+
+struct Fixture
+{
+	static Fixture* instance(){if(!inst)inst = new Fixture;return inst;}
+	int x;
+private:
+	Fixture(){inst=this;x=5;}
+	static Fixture *inst;
+};
+Fixture* Fixture::inst = nullptr;
+
+
+struct FixtureWrapper
+{
+	Fixture* instance;
+	FixtureWrapper(){instance = Fixture::instance();};
+};
+
+
+BOOST_AUTO_TEST_SUITE(Master)
+BOOST_GLOBAL_FIXTURE(FixtureWrapper)
 
 
 BOOST_AUTO_TEST_CASE( Jaccard_test )
 {
-	Jaccard jaccard;
-
-	std::cout << jaccard.usage() << std::endl;
+	FixtureWrapper fix;
+	std::cout << fix.instance->x << std::endl;
 	
+	
+	Jaccard jaccard;
+	// std::cout << jaccard.usage() << std::endl;
 	std::string cs0("");                                                 // 1 //
 	BOOST_CHECK_EQUAL( cs0.length(), (size_t)0 );
 	BOOST_CHECK( cs0.empty() );
@@ -46,8 +68,12 @@ BOOST_AUTO_TEST_CASE( Jaccard_test )
 
 BOOST_AUTO_TEST_CASE( ConsonniT1_test )
 {
-	std::cout << "ConsonniT1" << std::endl;
+	FixtureWrapper fix;
+	std::cout << fix.instance->x << std::endl;
+	
 	std::string cs0("");                                                 // 1 //
 	BOOST_CHECK_EQUAL( cs0.length(), (size_t)0 );
 	BOOST_CHECK( cs0.empty() );
 }
+
+BOOST_AUTO_TEST_SUITE_END()
