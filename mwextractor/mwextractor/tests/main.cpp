583b83edf03ce474404296b4f27fd2b3281ad4de
Igor <igor.danielewicz@gmail.com>
2017-11-09 10:30:50 +0100

    Frequency_test done!

--- mwextractor/mwextractor/tests/main.cpp
+++ mwextractor/mwextractor/tests/main.cpp
@@ -1,14 +1,9 @@
-
 #define BOOST_TEST_MODULE measures_test
-
-// #define BOOST_TEST_NO_MAIN
 #define BOOST_TEST_DYN_LINK
-#define BOOST_TEST_ALTERNATIVE_INIT_API
 
 #include <unordered_set>
 
 #include "../utils/Textual.h"
-#include "../function/FunctionFactory.h"
 #include "../structure/io/RelevantTupleSetLoader.h"
 #include "../structure/storage/RelevantTuple.h"
 #include "../structure/storage/ContingencyTable.h"
@@ -17,9 +12,12 @@
 #include "../structure/builder/CorporaParser.h"
 #include "../structure/builder/ContingencyTableGeneratorBuilder.h"
 
+#include "../function/FunctionFactory.h"
 #include "../function/association/AverageBigram.h"
 #include "../function/association/Frequency.h"
 #include "../function/association/Jaccard.h"
+#include "../function/association/PearsonsChiSquare.h"
+#include "../function/association/OddsRatio.h"
 #include "../function/association/ConsonniT1.h"
 #include "../function/association/ConsonniT2.h"
 
@@ -35,40 +33,55 @@
 
 
 using namespace function::association;
-
 using namespace structure::builder;
 using namespace structure::storage;
 
 struct Fixture
 {
-	static Fixture* instance(){if(!inst)inst = new Fixture;return inst;}
-	std::shared_ptr<ContingencyTableGenerator> getContTableGen() const {return contTableGen;}
-	std::shared_ptr<MatrixTupleStorage> getMatrixTupleStorage() const {return matrixTupleStorage;}
+	template<typename T>
+	using shPtr_vec = std::vector<std::shared_ptr<T>>;
+
+
+	static Fixture* instance(){if(!inst) inst = new Fixture; return inst;}
+	shPtr_vec<ContingencyTableGenerator> getContTableGen() const {return contTableGen;}
+	shPtr_vec<MatrixTupleStorage> getMatrixTupleStorage() const {return matrixTupleStorage;}
 private:
-	std::shared_ptr<ContingencyTableGenerator> contTableGen;
-	std::shared_ptr<MatrixTupleStorage> matrixTupleStorage;
+	shPtr_vec<ContingencyTableGenerator> contTableGen;
+	shPtr_vec<MatrixTupleStorage> matrixTupleStorage;
 	Fixture()
 	{
 		inst=this;
-		
-		std::vector<std::string> corporas
-			// {"/home/igor/MeWeX/mwextractor/data/test_data/medium.ccl",
-			// "/home/igor/MeWeX/mwextractor/data/test_data/ala.ccl",
-			// "/home/igor/MeWeX/mwextractor/data/test_data/text.ccl",
-			// "/home/igor/MeWeX/mwextractor/data/test_data/text.ccl",
-			{"/home/igor/MeWeX/mwextractor/data/test_data/ala.ccl"};
-	
 		auto const& tagset = Corpus2::get_named_tagset("nkjp");
 		auto wcclOperators = WcclOperatorLoader<Wccl::Bool>().
 			loadOperators(tagset, "/home/igor/MeWeX/mwextractor/data/test_data/2r.ccl", "");
-			// loadOperators(tagset, "/home/igor/MeWeX/mwextractor/data/patterns/adam_generalizations/2r.ccl", "");
-	
-		matrixTupleStorage = CorporaParser().parse(corporas, wcclOperators, tagset, "ccl");
+
+		std::vector<std::vector<std::string>> corporas
+			{{"/home/igor/MeWeX/mwextractor/data/test_data/0.ccl"},
+			{"/home/igor/MeWeX/mwextractor/data/test_data/1.ccl"},
+			{"/home/igor/MeWeX/mwextractor/data/test_data/2.ccl"},
+			{"/home/igor/MeWeX/mwextractor/data/test_data/3.ccl"},
+			{"/home/igor/MeWeX/mwextractor/data/test_data/4.ccl"},
+			{"/home/igor/MeWeX/mwextractor/data/test_data/5.ccl"},
+			{"/home/igor/MeWeX/mwextractor/data/test_data/1.ccl",
+			"/home/igor/MeWeX/mwextractor/data/test_data/2.ccl"},
+			{"/home/igor/MeWeX/mwextractor/data/test_data/1.ccl",
+			"/home/igor/MeWeX/mwextractor/data/test_data/2.ccl",
+			"/home/igor/MeWeX/mwextractor/data/test_data/3.ccl",
+			"/home/igor/MeWeX/mwextractor/data/test_data/4.ccl"}};
 		
-		contTableGen = ContingencyTableGeneratorBuilder().build(
-			matrixTupleStorage,
-			matrixTupleStorage->retrieveTupleIdVector()
-		);
+	
+		for(auto corpora : corporas)
+		{
+			matrixTupleStorage.push_back(
+				CorporaParser().parse(corpora, wcclOperators, tagset, "ccl")
+			);
+			
+			contTableGen.push_back(
+				ContingencyTableGeneratorBuilder().build(
+				matrixTupleStorage.back(),
+				matrixTupleStorage.back()->retrieveTupleIdVector()
+			));
+		}
 	}
 	static Fixture *inst;
 };
@@ -88,33 +101,90 @@ BOOST_GLOBAL_FIXTURE(FixtureWrapper)
 BOOST_AUTO_TEST_CASE( Frequency_test )
 {
 	FixtureWrapper fix;
-	auto matrix = fix.instance->getMatrixTupleStorage();
-	auto tableGen = fix.instance->getContTableGen();
-	
+	auto matrices = fix.instance->getMatrixTupleStorage();
+	auto tableGenerators = fix.instance->getContTableGen();
+	int i = -1;
+
+	double results[8][2] = {
+		{1,0},
+		{1,0},
+		{2,0},
+		{1,1},
+		{1,1},
+		{1,1},
+		{3,0},
+		{5,2}
+	};
+
 	std::cout << Frequency().usage() << std::endl;
-	for(auto tuple : matrix->retrieveTupleIdVector())
+	for(auto matrix : matrices)
 	{
-		auto table = tableGen->createTable(matrix->findTuple(tuple));
-		std::cout << Frequency().rankUsingTable(tuple, table) << " : \t";
-		std::cout << matrix->createTupleReprezentation(matrix->findTuple(tuple)) << std::endl;
+		int j = 0;
+		auto tableGen = tableGenerators[++i];
+		for(auto tuple : matrix->retrieveTupleIdVector())
+		{
+			auto table = tableGen->createTable(matrix->findTuple(tuple));
+			BOOST_CHECK_EQUAL( Frequency().rankUsingTable(tuple, table), results[i][j++]);
+			// std::cout << Frequency().rankUsingTable(tuple, table) << " : \t";
+			// std::cout << matrix->createTupleReprezentation(matrix->findTuple(tuple)) << std::endl;
+		}
 	}
 }
 
 BOOST_AUTO_TEST_CASE( Jaccard_test )
 {
 	FixtureWrapper fix;
-	auto matrix = fix.instance->getMatrixTupleStorage();
-	auto tableGen = fix.instance->getContTableGen();
-	
+	auto matrices = fix.instance->getMatrixTupleStorage();
+	auto tableGenerators = fix.instance->getContTableGen();
+	int i = -1;
+
+	double results[8][2] = {
+		{1.0/3.0,0},
+		{1.0/3.0,0},
+		{1.0/3.0,0},
+		{1.0/3.0,1.0/3.0},
+		{1.0/3.0,1.0/3.0},
+		{1.0/4.0,1.0/4.0},
+		{1.0/3.0,1.0/3.0},
+		{1.0/3.0,1.0/3.0}
+	};
+
 	std::cout << Jaccard().usage() << std::endl;
-	for(auto tuple : matrix->retrieveTupleIdVector())
+	for(auto matrix : matrices)
 	{
-		auto table = tableGen->createTable(matrix->findTuple(tuple));
-		std::cout << Jaccard().rankUsingTable(tuple, table) << " : \t";
-		std::cout << matrix->createTupleReprezentation(matrix->findTuple(tuple)) << std::endl;
+		int j = 0;
+		auto tableGen = tableGenerators[++i];
+		for(auto tuple : matrix->retrieveTupleIdVector())
+		{
+			auto table = tableGen->createTable(matrix->findTuple(tuple));
+			BOOST_CHECK_EQUAL( Jaccard().rankUsingTable(tuple, table), results[i][j++]);
+		}
 	}
 }
 
+BOOST_AUTO_TEST_CASE( Cout_test )
+{
+	FixtureWrapper fix;
+	auto matrices = fix.instance->getMatrixTupleStorage();
+	auto tableGenerators = fix.instance->getContTableGen();
+	int i = -1;
+
+	std::cout << "Manual Debug" << std::endl;
+	for(auto matrix : matrices)
+	{
+		int j = 0;
+		auto tableGen = tableGenerators[++i];
+		for(auto tuple : matrix->retrieveTupleIdVector())
+		{
+			auto table = tableGen->createTable(matrix->findTuple(tuple));
+			std::cout << matrix->createTupleReprezentation(matrix->findTuple(tuple)) << std::endl;
+			std::cout << "  " << Frequency().rankUsingTable(tuple, table) <<
+			"\t" << table.createReprezentation() << std::endl;
+		}
+		std::cout << "\n--------------------Matrix--------------------" << std::endl;
+	}
+}
+/*
 BOOST_AUTO_TEST_CASE( ConsonniT1_test )
 {
 	FixtureWrapper fix;
@@ -129,5 +199,35 @@ BOOST_AUTO_TEST_CASE( ConsonniT1_test )
 		std::cout << matrix->createTupleReprezentation(matrix->findTuple(tuple)) << std::endl;
 	}
 }
+
+BOOST_AUTO_TEST_CASE( PearsonsChiSquare_test )
+{
+	FixtureWrapper fix;
+	auto matrix = fix.instance->getMatrixTupleStorage();
+	auto tableGen = fix.instance->getContTableGen();
+	
+	std::cout << PearsonsChiSquare().usage() << std::endl;
+	for(auto tuple : matrix->retrieveTupleIdVector())
+	{
+		auto table = tableGen->createTable(matrix->findTuple(tuple));
+		std::cout << PearsonsChiSquare().rankUsingTable(tuple, table) << " <<>> ";
+		std::cout << matrix->createTupleReprezentation(matrix->findTuple(tuple)) << std::endl;
+	}
+}
+
+BOOST_AUTO_TEST_CASE( OddsRatio_test )
+{
+	FixtureWrapper fix;
+	auto matrix = fix.instance->getMatrixTupleStorage();
+	auto tableGen = fix.instance->getContTableGen();
+	
+	std::cout << OddsRatio().usage() << std::endl;
+	for(auto tuple : matrix->retrieveTupleIdVector())
+	{
+		auto table = tableGen->createTable(matrix->findTuple(tuple));
+		std::cout << OddsRatio().rankUsingTable(tuple, table) << " <<>> ";
+		std::cout << matrix->createTupleReprezentation(matrix->findTuple(tuple)) << std::endl;
+	}
+}*/
 
 BOOST_AUTO_TEST_SUITE_END()
